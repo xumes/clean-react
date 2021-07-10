b2ad1355a4be6ffdbdc045e095ffe8eacb5dc4a3
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-09 23:25:23 -0300

    test: ensure RemoteAuthentication throw InvalidCredentials error if HttpPostClient returns 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,8 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { InvalidCredentialError } from '@/domain/errors/invalid-credentials-error'
 import faker from 'faker'
 
 type SutTypes = {
@@ -30,5 +32,19 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('should throw InvalidCredentials error if HttpPostClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const errorResponse = {
+      error: 'invalid_credentials',
+      message: 'The user credentials were incorrect.'
+    }
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+      body: errorResponse
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new InvalidCredentialError(errorResponse))
   })
 })
