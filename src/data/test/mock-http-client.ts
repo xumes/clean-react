14a611c0c31d1c4ba02195f35784955ba3984b12
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-09 22:08:30 -0300

    refactor: forcing an error

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,7 +1,7 @@
 import { HttpPostClient } from '../protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
-  url?: string
+  url?: string;
   async post (url: string): Promise<void> {
     this.url = url
     return Promise.resolve()
