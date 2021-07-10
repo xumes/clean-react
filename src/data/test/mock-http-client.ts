908f768a4a39d8e3af33223dd86e9a2ca63ca326
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-09 22:05:48 -0300

    refactor: teting new version of husky

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -4,6 +4,6 @@ export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   async post (url: string): Promise<void> {
     this.url = url
-    return Promise.resolve();
+    return Promise.resolve()
   }
 }
