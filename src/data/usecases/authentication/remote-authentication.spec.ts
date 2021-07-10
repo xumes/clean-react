8a7925ffdecf886a8a1e6f1f8b712b9c0e8578aa
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-09 21:59:29 -0300

    refactor: fix lint

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,23 +2,23 @@ import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 type SutTypes = {
-    sut: RemoteAuthentication
-    httpPostClientSpy: HttpPostClientSpy
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
 }
 
 const makeSut = (url: string = 'any_url'): SutTypes => {
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
-    return {
-        sut,
-        httpPostClientSpy
-    }
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
 }
 
 describe('RemoteAuthentication', () => {
   test('should call HttpPostClient with correct URL', async () => {
     const url = 'any_url'
-    const { sut, httpPostClientSpy} = makeSut(url)
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
