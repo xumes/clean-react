e47e5e444fc633ed512ac09047a59904f05e08b0
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-10 23:28:43 -0300

    test: ensure AxiosHttpClient calls axios with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -16,10 +16,10 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('should call axios with correct URL and http verb', async () => {
+  test('should call axios with correct URL, http verb and body', async () => {
     const request = mockPostRequest()
     const sut = makeSut()
     await sut.post(request)
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
