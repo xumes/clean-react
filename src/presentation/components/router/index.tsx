fe3071af815a26e541337f058d65a7272058c4c4
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-19 22:30:26 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/components/router/index.tsx
+++ src/presentation/components/router/index.tsx
@@ -1,6 +1,6 @@
+import React from 'react'
 import Login from '@/presentation/pages/login'
 import '@/presentation/styles/global.scss'
-import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const Router: React.FC = () => {
--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,10 +1,10 @@
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import LoginHeader from '@/presentation/components/login-header'
 import Footer from '@/presentation/components/footer'
 import Input from '@/presentation/components/input'
 import FormStatus from '@/presentation/components/form-status'
 import Context from '@/presentation/contexts/form/form-context'
-import React, { useState } from 'react'
 
 type StateProps = {
   isLoading: boolean
@@ -24,7 +24,7 @@ const Login: React.FC = () => {
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="enter your e-mail" />
           <Input type="password" name="password" placeholder="enter your password" />
-          <button className={Styles.submit} type="submit">Login</button>
+          <button data-testid="submit" className={Styles.submit} type="submit" disabled>Login</button>
           <FormStatus />
         </form>
       </Context.Provider>
