6ecc70ca0f030842705e8ab0a7811d223391b340
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-22 16:43:57 -0300

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -45,13 +45,15 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
       setState({ ...state, isLoading: true })
 
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password,
         clientId: state.clientId,
         clientSecret: state.clientSecret,
         grantType: state.grantType
       })
+
+      localStorage.setItem('accessToken', account.accessToken)
     } catch (error) {
       setState({
         ...state,
