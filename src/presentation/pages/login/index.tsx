adde808c45c66c71e214368e7321f339a806a39e
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-22 09:14:43 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -6,19 +6,24 @@ import Input from '@/presentation/components/input'
 import FormStatus from '@/presentation/components/form-status'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
+import { Authentication } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
+  authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
     password: '',
     emailError: '',
     passwordError: '',
-    mainError: ''
+    mainError: '',
+    clientId: 'something',
+    clientSecret: 'something',
+    grantType: 'password'
   })
 
   useEffect(() => {
@@ -30,9 +35,17 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.password])
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     setState({ ...state, isLoading: true })
+
+    await authentication.auth({
+      email: state.email,
+      password: state.password,
+      clientId: state.clientId,
+      clientSecret: state.clientSecret,
+      grantType: state.grantType
+    })
   }
 
   return (
