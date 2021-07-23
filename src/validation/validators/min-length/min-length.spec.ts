bcfb96cb52a9a2e69171b86e1155b1b971d148be
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-22 22:30:43 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/min-length/min-length.spec.ts
+++ src/validation/validators/min-length/min-length.spec.ts
@@ -1,16 +1,21 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validation'
+import faker from 'faker'
+
+const makeSut = (): MinLengthValidation => {
+  return new MinLengthValidation(faker.database.column(), 5)
+}
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5)
-    const error = sut.validate('123')
+    const sut = makeSut()
+    const error = sut.validate(faker.datatype.string(3))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
-    const sut = new MinLengthValidation('field', 5)
-    const error = sut.validate('123456')
+    const sut = makeSut()
+    const error = sut.validate(faker.datatype.string(6))
     expect(error).toBeFalsy()
   })
 })
