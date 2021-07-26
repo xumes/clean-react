f4f4afdccbcff4e1743e0ca5734439dae023c682
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-26 20:07:10 -0300

    test: ensure ValidationComposite returns an error message if any validation fails

---
+++ src/validation/test/field-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '../protocols/field-validation'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+
+  constructor (readonly field: string) {}
+  validate (value: string): Error {
+    return this.error
+  }
+}
