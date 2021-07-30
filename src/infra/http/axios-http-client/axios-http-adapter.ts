bda98552647bcaa59dc8dda096c323d0c7e27d31
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-30 18:47:05 -0300

    chore: remove router from test

--- src/infra/http/axios-http-client/axios-http-adapter.ts
+++ src/infra/http/axios-http-client/axios-http-adapter.ts
@@ -5,7 +5,6 @@ export class AxiosHttpAdapter implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     let httpResponse: AxiosResponse<any>
     try {
-      console.log(params.body)
       httpResponse = await axios.post(params.url, params.body, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
