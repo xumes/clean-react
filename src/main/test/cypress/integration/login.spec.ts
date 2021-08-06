46e420b8da5ab0cd8f4ff033383346be31c1a4b8
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-08-05 23:08:26 -0300

    test: ensure login begins with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,19 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Required Field')
+      .should('contains.text', '😡')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Required Field')
+      .should('contains.text', '😡')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
