eaf907caf2ec4b54cde293ad9c72d3f978c14053
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-08-06 19:23:44 -0300

    test: ensure Input has focus on label click

---
+++ src/presentation/components/input/input.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import faker from 'faker'
+import { render, RenderResult, fireEvent } from '@testing-library/react'
+import Input from './input'
+import Context from '@/presentation/contexts/form/form-context'
+
+const makeSut = (fieldName: string): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name={fieldName} />
+    </Context.Provider>
+  )
+}
+
+describe('Input Component', () => {
+  test('Should focus input on label click', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field)
+    const label = sut.getByTestId(`${field}-label`)
+    fireEvent.click(label)
+    expect(document.activeElement).toBe(input)
+  })
+})
