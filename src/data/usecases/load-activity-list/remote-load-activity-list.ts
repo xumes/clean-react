c7a8bb92307872b9ac3098e27136616e9f116535
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-05 15:35:40 -0300

    feat: ensure RemoteLoadActivityList returns a list on 200

--- src/data/usecases/load-activity-list/remote-load-activity-list.ts
+++ src/data/usecases/load-activity-list/remote-load-activity-list.ts
@@ -1,16 +1,18 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { UnexpectedError } from '@/domain/errors'
+import { ActivityModel } from '@/domain/models'
+import { LoadActivityList } from '@/domain/usecases/load-activity-list'
 
-export class RemoteLoadActivityList {
+export class RemoteLoadActivityList implements LoadActivityList {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<ActivityModel[]>
   ) { }
 
-  async loadAll (): Promise<void> {
+  async loadAll (): Promise<ActivityModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body as ActivityModel[]
       default: throw new UnexpectedError()
     }
   }
