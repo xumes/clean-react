076c4d630025fbc8b900fe56ade8271dfb5cb94d
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-05 15:01:37 -0300

    test: ensure RemoteLoadActivityList throws UnexpectedError on 404

--- src/data/usecases/load-activity-list/remote-load-activity-list.spec.ts
+++ src/data/usecases/load-activity-list/remote-load-activity-list.spec.ts
@@ -38,4 +38,14 @@ describe('RemoteLoadActivityList', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notfound
+    }
+
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
