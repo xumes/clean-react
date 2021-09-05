f6c7c992a59625d0c13f266eba7e95aab02005d3
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-05 14:34:04 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-activity-list/remote-load-activity-list.spec.ts
+++ src/data/usecases/load-activity-list/remote-load-activity-list.spec.ts
@@ -2,12 +2,26 @@ import { HttpGetClientSpy } from '@/data/test'
 import { RemoteLoadActivityList } from './remote-load-activity-list'
 import faker from 'faker/locale/en_CA'
 
+type SutTypes = {
+  sut: RemoteLoadActivityList
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadActivityList(url, httpGetClientSpy)
+
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('RemoteLoadActivityList', () => {
   test('should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
 
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteLoadActivityList(url, httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
   })
