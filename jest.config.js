889475bd685458a68dd01fa8c7c9cede26813c92
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-06 21:31:37 -0300

    refactor: move Router to Main layer

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/presentation/components/router/**/*',
     '!**/*.d.ts',
     '!<rootDir>/src/**/index.ts'
   ],
