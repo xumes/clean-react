8b6d9fdc39243117822a2ab2c5db48452dbb310e
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-11 14:00:18 -0300

    test: ensure ActivityList component calls LoadActivityList

--- src/presentation/pages/activity-list/activity-list.spec.tsx
+++ src/presentation/pages/activity-list/activity-list.spec.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import ActivityList from '@/presentation/pages/activity-list/activity-list'
+import { LoadActivityList } from '@/domain/usecases/load-activity-list'
+import { ActivityModel } from '@/domain/models'
 
-const makeSut = (): void => {
-  render(<ActivityList />)
+class LoadActivityListSpy implements LoadActivityList {
+  callsCount = 0
+  async loadAll (): Promise<ActivityModel[]> {
+    this.callsCount++
+
+    return []
+  }
+}
+
+type SutTypes = {
+  loadActivityListSpy: LoadActivityListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadActivityListSpy = new LoadActivityListSpy()
+  render(<ActivityList loadActivityList={loadActivityListSpy} />)
+
+  return {
+    loadActivityListSpy
+  }
 }
 
 describe('ActivityLit Component', () => {
@@ -12,5 +32,11 @@ describe('ActivityLit Component', () => {
     const activityList = screen.getByTestId('activity-list')
 
     expect(activityList.querySelectorAll('li:empty').length).toBe(2)
+  })
+
+  test('Should call LoadSurveyList', () => {
+    const { loadActivityListSpy } = makeSut()
+
+    expect(loadActivityListSpy.callsCount).toBe(1)
   })
 })
