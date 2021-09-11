cf169d967ea5bc2f2ac022f6f66bd003ad0118ce
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-11 13:40:31 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/activity-list/activity-list.spec.tsx
+++ src/presentation/pages/activity-list/activity-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import ActivityList from '@/presentation/pages/activity-list/activity-list'
 
+const makeSut = (): void => {
+  render(<ActivityList />)
+}
+
 describe('ActivityLit Component', () => {
   test('Should display 2 empty items on start', () => {
-    render(<ActivityList />)
+    makeSut()
     const activityList = screen.getByTestId('activity-list')
 
     expect(activityList.querySelectorAll('li:empty').length).toBe(2)
