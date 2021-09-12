cb6c14faacb7fa2f716fd280b679d8b4462ee77a
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-12 19:07:49 -0300

    feat: ensure ActivityList render ActivityItems on success

--- src/presentation/pages/activity-list/activity-list.tsx
+++ src/presentation/pages/activity-list/activity-list.tsx
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './activity-list-styles.scss'
 import Header from '@/presentation/components/header/header'
 import Footer from '@/presentation/components/footer/footer'
-import { ActivityItemEmpty } from '@/presentation/pages/activity-list/components'
+import { ActivityItem, ActivityItemEmpty } from '@/presentation/pages/activity-list/components'
 import { LoadActivityList } from '@/domain/usecases/load-activity-list'
+import { ActivityModel } from '@/domain/models'
 
 type Props = {
   loadActivityList: LoadActivityList
 }
 
 const ActivityList: React.FC<Props> = ({ loadActivityList }: Props) => {
+  const [state, setstate] = useState({
+    activities: [] as ActivityModel[]
+  })
+
   useEffect(() => {
-    (async function () {
-      loadActivityList.loadAll()
-    })()
+    loadActivityList.loadAll()
+      .then(activities => setstate({ activities }))
   }, [])
 
   return (
@@ -22,7 +26,10 @@ const ActivityList: React.FC<Props> = ({ loadActivityList }: Props) => {
             <div className={Styles.contentWrap}>
                 <h2>Recent Activity</h2>
                 <ul data-testid="activity-list">
-                    <ActivityItemEmpty />
+                  {state.activities.length
+                    ? state.activities.map((activity: ActivityModel) => <ActivityItem key={activity.id} activity={activity} />)
+                    : <ActivityItemEmpty />
+                  }
                 </ul>
             </div>
             <Footer />
