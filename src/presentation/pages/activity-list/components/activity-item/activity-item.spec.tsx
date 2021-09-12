3b8fb183bd5f33cfa8a05195903f1474b3e97293
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-12 13:40:36 -0300

    test: ensure ActivityItem component renders with correct duration

--- src/presentation/pages/activity-list/components/activity-item/activity-item.spec.tsx
+++ src/presentation/pages/activity-list/components/activity-item/activity-item.spec.tsx
@@ -8,10 +8,12 @@ describe('ActivityItem component', () => {
   test('Should render with correct values', () => {
     const activity = mockActivityModel()
     activity.category = 'proposal-viewed'
+    activity.duration = 10
     render(<ActivityItem activity={activity} />)
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.proposalViewed)
     expect(screen.getByTestId('description')).toHaveTextContent(activity.description)
+    expect(screen.getByTestId('duration')).toHaveTextContent('for 10 seconds')
     expect(screen.getByTestId('time-ago')).toHaveTextContent(activity.formattedDateTime)
     expect(screen.getByTestId('date-time')).toHaveTextContent(activity.createdDateTime.toUTCString())
     expect(screen.getByTestId('action-button')).toHaveTextContent('Action')
@@ -24,6 +26,7 @@ describe('ActivityItem component', () => {
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.proposalCreated)
     expect(screen.getByTestId('description')).toHaveTextContent(activity.description)
+    expect(screen.getByTestId('duration')).toHaveTextContent('')
     expect(screen.getByTestId('time-ago')).toHaveTextContent(activity.formattedDateTime)
     expect(screen.getByTestId('date-time')).toHaveTextContent(activity.createdDateTime.toUTCString())
     expect(screen.getByTestId('action-button')).toHaveTextContent('Action')
@@ -36,6 +39,7 @@ describe('ActivityItem component', () => {
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.noIcon)
     expect(screen.getByTestId('description')).toHaveTextContent(activity.description)
+    expect(screen.getByTestId('duration')).toHaveTextContent('')
     expect(screen.getByTestId('time-ago')).toHaveTextContent(activity.formattedDateTime)
     expect(screen.getByTestId('date-time')).toHaveTextContent(activity.createdDateTime.toUTCString())
     expect(screen.getByTestId('action-button')).toHaveTextContent('Action')
