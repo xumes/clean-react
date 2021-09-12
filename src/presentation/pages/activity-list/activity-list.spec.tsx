35b2f08459d83fd7369d8cea81fdff6a59832323
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-12 19:29:04 -0300

    test: ensure ActivityList renders error message on failure

--- src/presentation/pages/activity-list/activity-list.spec.tsx
+++ src/presentation/pages/activity-list/activity-list.spec.tsx
@@ -4,6 +4,7 @@ import ActivityList from '@/presentation/pages/activity-list/activity-list'
 import { LoadActivityList } from '@/domain/usecases/load-activity-list'
 import { ActivityModel } from '@/domain/models'
 import { mockActivityListModel } from '@/domain/test'
+import { UnexpectedError } from '@/domain/errors'
 
 class LoadActivityListSpy implements LoadActivityList {
   callsCount = 0
@@ -20,8 +21,7 @@ type SutTypes = {
   loadActivityListSpy: LoadActivityListSpy
 }
 
-const makeSut = (): SutTypes => {
-  const loadActivityListSpy = new LoadActivityListSpy()
+const makeSut = (loadActivityListSpy = new LoadActivityListSpy()): SutTypes => {
   render(<ActivityList loadActivityList={loadActivityListSpy} />)
 
   return {
@@ -35,6 +35,7 @@ describe('ActivityLit Component', () => {
     const activityList = screen.getByTestId('activity-list')
 
     expect(activityList.querySelectorAll('li:empty')).toHaveLength(2)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
 
     await waitFor(() => activityList)
   })
@@ -53,5 +54,19 @@ describe('ActivityLit Component', () => {
     await waitFor(() => activityList)
 
     expect(activityList.querySelectorAll('li.activityItemWrap')).toHaveLength(3)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+  })
+
+  test('Should render error on failure', async () => {
+    const loadActivityListSpy = new LoadActivityListSpy()
+    const error = new UnexpectedError()
+    jest.spyOn(loadActivityListSpy, 'loadAll').mockRejectedValueOnce(error)
+
+    makeSut(loadActivityListSpy)
+
+    await waitFor(() => screen.getByRole('heading'))
+
+    expect(screen.queryByTestId('activity-list')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('error')).toHaveTextContent(error.message)
   })
 })
