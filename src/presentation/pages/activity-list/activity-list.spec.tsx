f5f7b2f8bfbb817f123bef5c064abc6a1e5bfc9e
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-12 19:08:25 -0300

    test: ensure ActivityList render ActivityItems on success

--- src/presentation/pages/activity-list/activity-list.spec.tsx
+++ src/presentation/pages/activity-list/activity-list.spec.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import ActivityList from '@/presentation/pages/activity-list/activity-list'
 import { LoadActivityList } from '@/domain/usecases/load-activity-list'
 import { ActivityModel } from '@/domain/models'
+import { mockActivityListModel } from '@/domain/test'
 
 class LoadActivityListSpy implements LoadActivityList {
   callsCount = 0
+  activities = mockActivityListModel()
+
   async loadAll (): Promise<ActivityModel[]> {
     this.callsCount++
 
-    return []
+    return this.activities
   }
 }
 
@@ -27,16 +30,28 @@ const makeSut = (): SutTypes => {
 }
 
 describe('ActivityLit Component', () => {
-  test('Should display 2 empty items on start', () => {
+  test('Should display 2 empty items on start', async () => {
     makeSut()
     const activityList = screen.getByTestId('activity-list')
 
-    expect(activityList.querySelectorAll('li:empty').length).toBe(2)
+    expect(activityList.querySelectorAll('li:empty')).toHaveLength(2)
+
+    await waitFor(() => activityList)
   })
 
-  test('Should call LoadSurveyList', () => {
+  test('Should call LoadSurveyList', async () => {
     const { loadActivityListSpy } = makeSut()
 
     expect(loadActivityListSpy.callsCount).toBe(1)
+
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  test('Should render ActivityItem on success', async () => {
+    makeSut()
+    const activityList = screen.getByTestId('activity-list')
+    await waitFor(() => activityList)
+
+    expect(activityList.querySelectorAll('li.activityItemWrap')).toHaveLength(3)
   })
 })
