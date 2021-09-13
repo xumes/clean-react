ed6779dd9b2f923a7820c81f4dcc080c84f69da8
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-13 10:46:03 -0300

    feat: ensure AuthorizeHttpGetClientDecorator add access token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -8,7 +8,16 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   ) {}
 
   async get (params: HttpGetParams): Promise<HttpResponse<any>> {
-    this.getStorage.get('account')
+    const account = this.getStorage.get('account')
+
+    if (account?.accessToken) {
+      Object.assign(params, {
+        headers: {
+          Authorization: `Bearer ${account.accessToken}`
+        }
+      })
+    }
+
     await this.httpGetClient.get(params)
 
     return null
