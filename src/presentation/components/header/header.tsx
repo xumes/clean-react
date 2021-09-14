86958fc84af7320dc3dc5994da33a1fb2b49540b
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-14 15:20:05 -0300

    feat: ensure Header Component calls setCurrentAccount with null on logout

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import Logo from '@/presentation/components/logo/logo'
 import Styles from './header-styles.scss'
+import { ApiContext } from '@/presentation/contexts'
+import { useHistory } from 'react-router-dom'
 
 const Header: React.FC = () => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
+  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault()
+
+    setCurrentAccount(undefined)
+
+    history.replace('/login')
+  }
+
   return (
     <header className={Styles.headerWrap}>
         <div className={Styles.headerContent}>
             <Logo />
             <div className={Styles.logoutWrap}>
                 <span>Reginaldo Santos</span>
-                <a href="#">Logout</a>
+                <a data-testid="logout" href="#" onClick={logout}>Logout</a>
             </div>
         </div>
     </header>
