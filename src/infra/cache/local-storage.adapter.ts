4c4a669bd4c7a2486a8f994025b78e1e3fc172fe
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-14 14:52:02 -0300

    feat: ensure LocalStorageAdapter calls removeItem if value is null or undefined

--- src/infra/cache/local-storage.adapter.ts
+++ src/infra/cache/local-storage.adapter.ts
@@ -2,6 +2,10 @@ import { SetStorage, GetStorage } from '@/data/protocols/cache'
 
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   set (key: string, value: object): void {
+    if (!value) {
+      localStorage.removeItem(key)
+    }
+
     localStorage.setItem(key, JSON.stringify(value))
   }
 
--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,4 +1,3 @@
-import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage.adapter'
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
@@ -13,12 +12,6 @@ describe('CurrentAccountAdapter', () => {
     setCurrentAccountAdapter(account)
 
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
   })
 
   test('Should call LocalStorageAdapter.get with correct value', () => {
