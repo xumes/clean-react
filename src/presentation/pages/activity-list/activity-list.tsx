323f0e6a777f104f1352f31afc047b923c5e873a
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-19 11:33:49 -0300

    feat: ensure ActivityList logout user on AccessDeniedError

--- src/presentation/pages/activity-list/activity-list.tsx
+++ src/presentation/pages/activity-list/activity-list.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
 import Styles from './activity-list-styles.scss'
+import { LoadActivityList } from '@/domain/usecases/load-activity-list'
+import { AccessDeniedError } from '@/domain/errors'
 import Header from '@/presentation/components/header/header'
 import Footer from '@/presentation/components/footer/footer'
 import { ActivittListItem, ActivityContext, ActivityError } from '@/presentation/pages/activity-list/components'
-import { LoadActivityList } from '@/domain/usecases/load-activity-list'
+import { ApiContext } from '@/presentation/contexts'
 
 type Props = {
   loadActivityList: LoadActivityList
 }
 
 const ActivityList: React.FC<Props> = ({ loadActivityList }: Props) => {
+  const history = useHistory()
+  const { setCurrentAccount } = useContext(ApiContext)
   const [state, setState] = useState({
     activities: [] as LoadActivityList.Model[],
     error: '',
@@ -19,7 +24,14 @@ const ActivityList: React.FC<Props> = ({ loadActivityList }: Props) => {
   useEffect(() => {
     loadActivityList.loadAll()
       .then(activities => setState({ ...state, activities }))
-      .catch(error => setState({ ...state, error: error.message }))
+      .catch(error => {
+        if (error instanceof AccessDeniedError) {
+          setCurrentAccount(undefined)
+          history.replace('/login')
+        } else {
+          setState({ ...state, error: error.message })
+        }
+      })
   }, [state.reload])
 
   return (
