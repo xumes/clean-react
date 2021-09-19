9069543cc9fa8db8523a6502546caa31b29f7dab
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-09-19 15:05:16 -0300

    test: ensure ActivityList logout on AccessDeniedError

--- src/main/test/cypress/integration/activity-list.spec.ts
+++ src/main/test/cypress/integration/activity-list.spec.ts
@@ -6,6 +6,7 @@ describe('ActivityList', () => {
   beforeEach(() => {
     Helper.setLocalStorageItem('account', { access_token: faker.datatype.uuid() })
   })
+
   it('Should display error on UnexpectedError', () => {
     const errorMessage = 'Something wrong happened. Please, try again later.'
     Http.mockUnexpectedError(errorMessage)
@@ -13,5 +14,14 @@ describe('ActivityList', () => {
     cy.visit('/')
 
     cy.getByTestId('error').should('contain.text', errorMessage)
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    const errorMessage = 'Something wrong happened. Please, try again later.'
+    Http.mockAccessDeniedError(errorMessage)
+
+    cy.visit('/')
+
+    Helper.testUrl('/login')
   })
 })
--- src/main/test/cypress/support/activity-list-mocks.ts
+++ src/main/test/cypress/support/activity-list-mocks.ts
@@ -1,3 +1,4 @@
 import * as Http from './http-mocks'
 
 export const mockUnexpectedError = (errorMessage: string): void => Http.mockServerError('/activity', 'GET', errorMessage)
+export const mockAccessDeniedError = (errorMessage: string): void => Http.mockForbiddenError(/activity/, 'GET')
